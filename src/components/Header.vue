d121cc874e0e384bfe9e4342a4a573f052e94843
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-08-29 14:35:19 +0900

    add

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,39 +2,11 @@
   <header>
     <v-app-bar
       app
-      color="primary"
-      dark
+      color="#E3F2FD"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <div class="d-flex align-center">
+        <v-img src="../assets/myLogo.png" />
+    </div>
     </v-app-bar>
   </header>
 </template>
