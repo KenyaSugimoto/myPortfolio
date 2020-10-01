b72238b34f0e2f3a01fc72500e9a769a524e156c
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-10-01 15:47:46 +0900

    add

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -31,31 +31,31 @@ export default {
     return {
       items: [
         {
-          src: require("./../assets/panpies.jpg"),
+          src: require("@/assets/panpies.jpg"),
           episode: "野球の大会で念願の優勝した時の写真です。私のサークルチームは野球経験者ばかりではないですが強豪チームにも負けないチーム力があります！"
         },
         {
-          src: require("./../assets/baseball.jpg"),
+          src: require("@/assets/baseball.jpg"),
           episode: "関西の選抜チームの投手として、ナゴヤドームで関東選抜チームと試合をしました。試合にも勝ち、両親の前で全力プレーを見せることができました！"
         },
         {
-          src: require("./../assets/animal.png"),
+          src: require("@/assets/animal.png"),
           episode: "動物が大好きです！歩いている動物を見つけると一緒に写真を撮りたくなります！"
         },
         {
-          src: require("./../assets/animal2.jpg"),
+          src: require("@/assets/animal2.jpg"),
           episode: "猫に癒されたかったので岡山県の真鍋島（猫島）に行きました！"
         },
         {
-          src: require("./../assets/onji.jpg"),
+          src: require("@/assets/onji.jpg"),
           episode: "学部で成績優秀者（上位2%以内）に選ばれた時の写真です。右の友達と一緒に勉強して2人で選ばれることができました！"
         },
         {
-          src: require("./../assets/auth.jpg"),
+          src: require("@/assets/auth.jpg"),
           episode: "オーストラリア留学に行った時の写真です。ホストファミリーとその友人家族とディナーをしました。"
         },
         {
-          src: require("./../assets/q1.jpg"),
+          src: require("@/assets/q1.jpg"),
           episode: "南半球で一番高いビルであるQ1のスカイウォーク体験をしました。怖かったです。"
         },
       ],
